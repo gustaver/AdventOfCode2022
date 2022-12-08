d863d994b6f58f07bd4456001763fab8dbd4bc81
Gustave Rousselet <gustave.rousselet@icloud.com>
2022-12-08 22:58:16 +0000

    Day 7

--- src/solutions/seven.rs
+++ src/solutions/seven.rs
@@ -2,15 +2,11 @@ use itertools::Itertools;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
-enum FSNode {
-    Directory(HashMap<String, FSNode>),
-    File(usize)
-}
+const TOTAL_SPACE: usize = 70000000;
+const UPDATE_SPACE: usize = 30000000;
 
-fn build_filesystem(commands: &Vec<&str>) -> HashMap<String, FSNode> {
+fn build_filesystem(commands: &Vec<&str>) -> HashMap<PathBuf, usize> {
     let mut root = HashMap::new();
-    root.insert(String::from("/"), FSNode::Directory(HashMap::new()));
-
     let mut path = PathBuf::new();
 
     for &command in commands {
@@ -32,15 +28,11 @@ fn build_filesystem(commands: &Vec<&str>) -> HashMap<String, FSNode> {
             },
             "ls" => {
                 let output = command.lines().dropping(1).collect::<Vec<_>>();
-                let current = &root;
-                println!("path: {:?}", path.to_str().unwrap());
-                for dir in path.iter() {
-                    println!("{:?}", dir);
-                    let current = match current.get(dir.to_str().unwrap()).unwrap() {
-                        FSNode::Directory(map) => map,
-                        _ => unreachable!()
-                    };
-                }
+                let size: usize = output.iter().filter_map(|&l| {
+                    let (size, _) = l.split_once(" ").unwrap();
+                    size.parse::<usize>().ok()
+                }).sum();
+                root.insert(path.clone(), size);
             },
             _ => unreachable!()
         }
@@ -52,5 +44,15 @@ fn build_filesystem(commands: &Vec<&str>) -> HashMap<String, FSNode> {
 pub fn solve(input: &str) -> (usize, usize) {
     let commands = input.split("$ ").dropping(1).collect::<Vec<_>>();
     let fs = build_filesystem(&commands);
-    (0, 0)
+    let sizes = fs.keys().map(|path| {
+        fs.iter().filter_map(|(p, s)| p.to_str().unwrap().contains(path.to_str().unwrap()).then_some(s)).sum::<usize>()
+    }).collect::<Vec<_>>();
+
+    let p1: usize = sizes.iter().filter_map(|&s| (s <= 100000).then_some(s)).sum();
+
+    let used_space = fs.values().sum::<usize>();
+    let remaining_space = TOTAL_SPACE - used_space;
+    let &p2 = sizes.iter().filter(|&s| remaining_space + s > UPDATE_SPACE).sorted().next().unwrap();
+    
+    (p1, p2)
 }
