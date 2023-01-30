89b04dd14533bbc90b44693265dd73c922f243d3
Gustave Rousselet <grousselet@palantir.com>
2023-01-30 16:26:07 +0000

    Day 25

--- src/solutions/twentyfive.rs
+++ src/solutions/twentyfive.rs
@@ -30,10 +30,11 @@ fn add(x: & str, y: &str) -> String {
     let n = x.len().max(y.len()) + 1;
     let x_pad = format!("{x:0>width$}", width=n);
     let y_pad = format!("{y:0>width$}", width=n);
-    zip(x_pad.chars().rev(), y_pad.chars().rev()).fold((0, "".to_string()), |(c, s), (a, b)| {
+    let z = zip(x_pad.chars().rev(), y_pad.chars().rev()).fold((0, "".to_string()), |(c, s), (a, b)| {
         let (carry, sum) = add_digits(a, b, c);
         (carry, format!("{sum}{s}"))
-    }).1
+    }).1;
+    if z.starts_with("0") { z[1..].to_string() } else { z }
 }
 
 pub fn solve(input: &str) -> (String, &str) {
